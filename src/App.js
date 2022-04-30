a7afba5fb7c4e6a3ab09351a1df35dfd49f1b4e3
Nana Kumi <baider11222gmail.com>
2022-04-30 13:48:50 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import {Link,Route,Switch} from 'react-router-dom'; 
+import Form from './Form'
+import Home from './Home'
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="header">
+    <h1>Lambda Pizza We put the P in Piźźà</h1>
+    <p>Safe space for crust lovers and crust haters!</p>
+    <p>Disclaimer: All our pizza is made from vegan products</p>
+    <nav>
+    <Link to="/"><button id="home-button">Home</button></Link>
+    <Link to="/pizza"><button id="order-pizza">Order Pizza!</button></Link>
+    </nav>
+    <Switch>
+    <Route path="/pizza" component={Form} />
+      <Route path="/" component={Home} />
+    </Switch>
+  </div>
   );
 };
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Home() {
+
+    return (
+
+        <div className="homePage">
+            <nav>
+                <h1> Lambda's Favorite </h1>
+                <img src="https://veganhuggs.com/wp-content/uploads/2017/10/vegan-pizza-recipe-22-1170x780.jpg" />
+                <h1>Vegan Rocky Road Dessert Pizza</h1>
+                <img src="https://crumbsandcaramel.com/wp-content/uploads/2020/05/Blog-Crop-Vegan-Nutella-Rocky-Road-Dessert-Pizza-Slices.jpg.webp" />
+
+            </nav>
+
+        </div>
+
+    );
+
+}
+
+export default Home;
